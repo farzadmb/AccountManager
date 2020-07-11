12a34571bd48caedd877f0b1dd73189695b5146b
Fred Momeni <fred.momeni@3plearning.com>
2020-07-11 22:21:56 +1000

    Adding extensions

---
+++ AccountManager.Application/Extensions/UserDtoExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AccountManager.Application.DTOs;
+using AccountManager.Data.Models;
+
+namespace AccountManager.Application.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="UserDto"/>
+    /// </summary>
+    public static class UserDtoExtensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Converts <see cref="UserDto"/> to <see cref="User"/>
+        /// </summary>
+        /// <param name="user">
+        /// The user
+        /// </param>
+        /// <returns>
+        /// The <see cref="User"/> object
+        /// </returns>
+        public static User ToUser(this UserDto user)
+        {
+            if (user == null)
+            {
+                return null;
+            }
+
+            return new User()
+                       {
+                           Id = user.Id,
+                           Name = user.Name,
+                           Email = user.Email,
+                           Salary = user.Salary,
+                           Expenses = user.Expenses
+                       };
+        }
+
+        #endregion
+    }
+}
---
+++ AccountManager.Application/Extensions/UserExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AccountManager.Application.DTOs;
+using AccountManager.Data.Models;
+
+namespace AccountManager.Application.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="User"/>
+    /// </summary>
+    public static class UserExtensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Converts <see cref="User"/> to <see cref="UserDto"/>
+        /// </summary>
+        /// <param name="user">
+        /// The user
+        /// </param>
+        /// <returns>
+        /// The <see cref="UserDto"/> object
+        /// </returns>
+        public static UserDto ToUserDto(this User user)
+        {
+            if (user == null)
+            {
+                return null;
+            }
+
+            return new UserDto()
+                       {
+                           Id = user.Id,
+                           Name = user.Name,
+                           Email = user.Email,
+                           Salary = user.Salary,
+                           Expenses = user.Expenses
+                       };
+        }
+
+        /// <summary>
+        /// Converts a list of <see cref="User"/> to a list of <see cref="UserDto"/>
+        /// </summary>
+        /// <param name="users">
+        /// The users
+        /// </param>
+        /// <returns>
+        /// The list of <see cref="UserDto"/> object
+        /// </returns>
+        public static IEnumerable<UserDto> ToUserDto(this IEnumerable<User> users)
+        {
+            return users?.Select(u => u.ToUserDto());
+        }
+
+        #endregion
+    }
+}
