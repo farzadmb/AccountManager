4e6415d5171983128c3662cb356b532555c79cf6
Fred Momeni <fred.momeni@3plearning.com>
2020-07-11 22:22:18 +1000

    Adding User services

---
+++ AccountManager.Application/IUserService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using AccountManager.Application.DTOs;
+
+namespace AccountManager.Application
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// Returns the list of all users
+        /// </summary>
+        /// <returns>
+        /// The list of <see cref="UserDto"/>
+        /// </returns>
+        IEnumerable<UserDto> GetAllUsers();
+
+        /// <summary>
+        /// Returns a user
+        /// </summary>
+        /// <param name="email">
+        /// The email of the user
+        /// </param>
+        /// <returns>
+        /// The <see cref="UserDto"/>
+        /// </returns>
+        UserDto GetUser(string email);
+
+        /// <summary>
+        /// Adds the given user to the database
+        /// </summary>
+        /// <param name="user">
+        /// The new user
+        /// </param>
+        /// <returns>
+        /// </returns>
+        Task AddUser(UserDto user);
+    }
+}
---
+++ AccountManager.Application/UserServices.cs
@@ -0,0 +1,56 @@
+﻿using AccountManager.Data.DbHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AccountManager.Application.DTOs;
+using AccountManager.Application.Extensions;
+
+namespace AccountManager.Application
+{
+    /// <summary>
+    /// The implementation for <see cref="IUserService"/>
+    /// </summary>
+    public class UserServices : IUserService
+    {
+        #region Fields
+
+        private readonly IUserDbHandler userDbHandler;
+
+        #endregion
+
+        #region Constructor
+
+        public UserServices(IUserDbHandler userDbHandler)
+        {
+            this.userDbHandler = userDbHandler;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public IEnumerable<UserDto> GetAllUsers()
+        {
+            return userDbHandler.GetUsers().ToUserDto();
+        }
+
+        public UserDto GetUser(string email)
+        {
+            return userDbHandler.GetUsers().FirstOrDefault(u => u.Email == email).ToUserDto();
+        }
+
+        public async Task AddUser(UserDto user)
+        {
+            if (user == null)
+            {
+                throw new Exception();
+            }
+
+            await userDbHandler.AddUserAsync(user.ToUser());
+        }
+
+        #endregion
+    }
+}
