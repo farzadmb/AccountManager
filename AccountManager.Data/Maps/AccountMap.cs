fa45ebf85d2f9f620e19dec773c99739aac4e7b4
Fred Momeni <fred.momeni@3plearning.com>
2020-07-11 20:38:21 +1000

    Adding mappers for models

---
+++ AccountManager.Data/Maps/AccountMap.cs
@@ -0,0 +1,33 @@
+﻿using AccountManager.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccountManager.Data.Maps
+{
+    /// <summary>
+    /// The mapper for <see cref="Account"/>
+    /// </summary>
+    public class AccountMap
+    {
+        #region Constructor
+
+        /// <summary>
+        /// The default constructor
+        /// </summary>
+        /// <param name="entityBuilder">
+        /// The entity builder
+        /// </param>
+        public AccountMap(EntityTypeBuilder<Account> entityBuilder)
+        {
+            entityBuilder.HasKey(a => a.Id);
+            entityBuilder.ToTable("account");
+
+            entityBuilder.Property(a => a.Id).HasColumnName("id");
+            entityBuilder.Property(a => a.UserId).IsRequired().HasColumnName("userId");
+            entityBuilder.Property(a => a.CreationDate).HasColumnName("date");
+            entityBuilder.Property(a => a.IsActive).HasColumnName("isActive");
+        }
+
+        #endregion
+    }
+}
---
+++ AccountManager.Data/Maps/UserMap.cs
@@ -0,0 +1,34 @@
+﻿using AccountManager.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccountManager.Data.Maps
+{
+    /// <summary>
+    /// The mapper for <see cref="User"/>
+    /// </summary>
+    public class UserMap
+    {
+        #region Constructor
+
+        /// <summary>
+        /// The default constructor
+        /// </summary>
+        /// <param name="entityBuilder">
+        /// The entity builder
+        /// </param>
+        public UserMap(EntityTypeBuilder<User> entityBuilder)
+        {
+            entityBuilder.HasKey(a => a.Id);
+            entityBuilder.ToTable("user");
+
+            entityBuilder.Property(a => a.Id).HasColumnName("id");
+            entityBuilder.Property(a => a.Name).IsRequired().HasColumnName("name");
+            entityBuilder.Property(a => a.Email).IsRequired().HasColumnName("email");
+            entityBuilder.Property(a => a.Salary).HasColumnName("salary");
+            entityBuilder.Property(a => a.Expenses).HasColumnName("expenses");
+        }
+
+        #endregion
+    }
+}
