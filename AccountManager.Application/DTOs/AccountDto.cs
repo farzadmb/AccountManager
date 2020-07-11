d626dcf342f8c5948cd1b1f00707486bef94e9e4
Fred Momeni <fred.momeni@3plearning.com>
2020-07-11 22:21:46 +1000

    Adding DTOs

---
+++ AccountManager.Application/DTOs/AccountDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AccountManager.Application.DTOs
+{
+    public class AccountDto
+    {
+        /// <summary>
+        /// Gets or sets the Id of the account
+        /// </summary>
+        public uint Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Id of the user
+        /// </summary>
+        public UserDto User { get; set; }
+
+        /// <summary>
+        /// Gets or sets the creation date
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the account is active
+        /// </summary>
+        public bool IsActive { get; set; }
+    }
+}
---
+++ AccountManager.Application/DTOs/UserDto.cs
@@ -0,0 +1,30 @@
+﻿namespace AccountManager.Application.DTOs
+{
+    public class UserDto
+    {
+        /// <summary>
+        /// Gets or sets the Id od the user
+        /// </summary>
+        public uint Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email of the user
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the salary of the user
+        /// </summary>
+        public uint Salary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expenses of the user
+        /// </summary>
+        public uint Expenses { get; set; }
+    }
+}
