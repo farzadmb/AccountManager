c83a43d37b87925bb43176e87850cf7fc0c2799c
Fred Momeni <fred.momeni@3plearning.com>
2020-07-11 20:23:45 +1000

    Adding models

---
+++ AccountManager.Data/Models/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountManager.Data.Models
+{
+    /// <summary>
+    /// The Account model
+    /// </summary>
+    public class Account
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the Id of the account
+        /// </summary>
+        public uint Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Id of the user
+        /// </summary>
+        public uint UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the creation date
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the account is active
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        #endregion
+    }
+}
---
+++ AccountManager.Data/Models/User.cs
@@ -0,0 +1,37 @@
+﻿namespace AccountManager.Data.Models
+{
+    /// <summary>
+    /// The User model
+    /// </summary>
+    public class User
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the Id od the user
+        /// </summary>
+        public uint Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email of the user
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the salary of the user
+        /// </summary>
+        public uint Salary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expenses of the user
+        /// </summary>
+        public uint Expenses { get; set; }
+
+        #endregion
+    }
+}
